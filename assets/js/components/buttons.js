d0e8f1f1a0b1747cbd8be83b62fa2b4039402c56
Tom Sixma <sixmatom@gmail.com>
2021-12-20 12:08:45 +0100

    added text to background color adjustment

--- assets/js/components/buttons.js
+++ assets/js/components/buttons.js
@@ -15,16 +15,14 @@ Vue.component('buttonstyle', {
         addToCart(id) {
             this.$root.$emit('add-to-cart', id)
         },
-        calcTextColor(hex) {
-            let threshold = 130; // about 256/2 - lower values results into darker text on dark background
-            let colorBrightness = ((hexToRed(hex) * 299) + (hexToGreen(hex) * 587) + (hexToBlue(hex) * 114)) / 1000;
-          
-            function cleanHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
-            function hexToRed(h) {return parseInt((cleanHex(h)).substring(0,2),16)}
-            function hexToGreen(h) {return parseInt((cleanHex(h)).substring(2,4),16)}
-            function hexToBlue(h) {return parseInt((cleanHex(h)).substring(4,6),16)}
-          
-            return (colorBrightness > threshold) ? "#000000" : "#ffffff"	
+        calcTextColor(hexcolor){
+            hexcolor = hexcolor.replace("#", "");
+            var r = parseInt(hexcolor.substr(0,2),16);
+            var g = parseInt(hexcolor.substr(2,2),16);
+            var b = parseInt(hexcolor.substr(4,2),16);
+            var yiq = ((r*299)+(g*587)+(b*114))/1000;
+            return (yiq >= 128) ? 'black' : 'white';
+        
           
         },
        
@@ -34,7 +32,10 @@ Vue.component('buttonstyle', {
     
     template:`
     <div class="btn-group">
-    <button :style ="{'background-color':[color_data.bGC], 'color': calcTextColor(toString([color_data.bGC]))}" @click="addToCart([color_data.id],console.log([color_data.id]))" >{{color_data.name}}</button>
-       ` 
+    <button :style ="{'background-color':[color_data.bGC], 'color': calcTextColor(color_data.bGC)}" @click="addToCart([color_data.id],console.log([color_data.id]))" >{{color_data.name}}</button>
+    </div>   
+    ` 
     
 })
+Vue.config.devtools = true
+Vue.config.productionTip = false
